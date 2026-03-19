e6b66e01bac61f0ea012bee05af7bfead38fc9e6
lonewolfyx <877085893@qq.com>
2026-03-19 23:25:15 +0800

    feat(cli): add standard uni-app template download feature

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,8 @@
+import { spinner } from '@clack/prompts'
 import { createMain, defineCommand } from 'citty'
 import { commandArgs } from '@/args.ts'
 import { resolveConfig } from '@/config.ts'
+import { downloadStandardUniAppTemplate } from '@/standard.ts'
 import { description, name, version } from '../package.json'
 
 const command = defineCommand({
@@ -18,6 +20,11 @@ const command = defineCommand({
     args: commandArgs,
     async run({ args }) {
         const config = await resolveConfig(args)
+        const s = spinner()
+
+        s.start('正在收集最新的项目依赖')
+        await downloadStandardUniAppTemplate(config)
+        s.stop('')
         console.log(config)
     },
 })
