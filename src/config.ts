4c0367934156277c3a8ab65e9dc03f92c8620945
lonewolfyx <877085893@qq.com>
2026-03-19 23:24:24 +0800

    feat(config): add template path resolution and update config types

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,19 @@
 import type { ParsedArgs } from 'citty'
 import type { commandArgs } from '@/args.ts'
-import type { IOptions } from '@/types'
+import type { IResolveConfig } from '@/types'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 type CommandArgs = ParsedArgs<typeof commandArgs>
 
 const defaultConfig = {
     cwd: process.cwd(),
+    templatePath: resolve(dirname(fileURLToPath(import.meta.url)), '../'),
     dependencies: {},
     devDependencies: {},
 }
 
-export const resolveConfig = async (options: Partial<CommandArgs>): Promise<IOptions> => {
+export const resolveConfig = async (options: Partial<CommandArgs>): Promise<IResolveConfig> => {
     const { loadConfig } = await import('c12')
 
     const { config } = await loadConfig({
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,6 @@
-export interface IOptions {
+export interface IResolveConfig {
     cwd: string
+    templatePath: string
     dependencies: Record<string, string>
     devDependencies: Record<string, string>
 }
